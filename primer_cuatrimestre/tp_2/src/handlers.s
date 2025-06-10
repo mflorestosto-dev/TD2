520428b6809557ffa0576acbc59ef2ad89ea89e3
mflorestosto <mflorestosto@gmail.com>
2025-06-10 18:55:22 -0300

    agrego progreso

---
+++ primer_cuatrimestre/tp_2/src/handlers.s
@@ -0,0 +1,37 @@
+/*
+Este archivo define los manejadores de excepciones (handlers). 
+Cada sección (b .) significa que cuando ocurre esa excepción, el procesador 
+se queda en un bucle infinito (esperando que el usuario lo depure).
+ */
+
+
+.global undef_handler
+.global swi_handler
+.global prefetch_abort
+.global data_abort
+.global reserved_handler
+.global irq_handler
+.global fiq_handler
+
+.section .handlers, "ax"
+
+undef_handler:       
+    b .
+
+swi_handler:         
+    b .
+
+prefetch_abort:      
+    b .
+
+data_abort:          
+    b .
+
+reserved_handler:    
+    b .
+
+irq_handler:         
+    b .
+
+fiq_handler:         
+    b .
