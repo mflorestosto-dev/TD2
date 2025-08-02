a73299c009fa5777e3fff9543c9a6a56f71e784d
mflorestosto <mflorestosto@gmail.com>
2025-08-02 10:48:55 -0300

    Subo tp0 y tp1 bien, ahora si

--- primer_cuatrimestre/tp_1/src/board_init.c
+++
@@ -1,9 +0,0 @@
-#include "gic.h"    // El header de gic.c 
-#include "timer.h"  // El header de timer.c
-
-void board_init(void) {
-    
-    //Inicializacion de gic y timer
-    __gic_init();
-    __timer_init();
-}
